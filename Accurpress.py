ab9f5cb7e30a9318b3c816855753b539be71a275
Umidah <alantsin92@gmail.com>
2017-12-06 11:05:45 -0800

    Finished formatting output

--- Accurpress.py
+++ Accurpress.py
@@ -14,8 +14,8 @@ print ("Number of numbers: " + str(n))
 # If n = 0, uses test values
 if n == 0:
     # Adjust n and numbers for your own purposes
-    n = 9
-    numbers = [0.75, 1, 1.25, 2.25, 4, 8.5, 17, 34, 75.25]
+    n = 11
+    numbers = [0.75, 1, 1.25, 2.25, 4, 5.25, 8.5, 12, 17, 48, 56]
     print("Using test values:")
     for num in numbers:
         print(Fraction(num))        
@@ -44,12 +44,8 @@ else:
         
 # Set up for output
 counter = 1
-# Output 1: Equations with unsorted sums
-output_unsorted = []
-# Output 2: Sorted sums only
-output_sums = []
-# Output 3: Equations and sorted sums
-output_sorted = []
+# List to keep track of data. [0] is count, [1] is equation, [2] is sum
+output = []
 
 # Summation loops
 # First loop to keep track of total numbers to sum
@@ -58,8 +54,8 @@ for numbersToAdd in range(1, n + 1):
     for startIndex in range(0, n):
         # If only one number, the sum is that number
         if numbersToAdd == 1:
-            output_unsorted.append("%s = %s" % (str(numbers[startIndex]), str(numbers[startIndex])))
-            print("Sum #%s: %s" % (str(counter), output_unsorted[counter - 1]))
+            output.append(("Sum #%s: " % str(counter), str(numbers[startIndex]), str(numbers[startIndex])))
+            print("%s: %s = %s" % (output[counter - 1][0], output[counter - 1][1], output[counter - 1][2]))
             counter += 1
         # Conditional to determine if this combination i and j would exceed the number of numbers
         elif numbersToAdd + startIndex > n:
@@ -86,10 +82,26 @@ for numbersToAdd in range(1, n + 1):
                     for endIndex in range(currentIndex, n):
                         tempSumStringFinal = tempSumString + " + " + str(numbers[endIndex])
                         total = tempSum + numbers[endIndex]
-                        print("Sum #" + str(counter) + ": " + tempSumStringFinal + " = " + str(total))
+                        output.append(("Sum #%s: " % str(counter), tempSumStringFinal, str(total)))
+                        print("%s: %s = %s" % (output[counter - 1][0], output[counter - 1][1], output[counter - 1][2]))
                         counter += 1
                 
+# Write results to files
+print("Now creating files for output")
+f = open('output_unsorted.txt', 'w')
+for i in range(0, counter - 1):
+    f.write("%s: %s = %s\n" % (output[i][0], output[i][1], output[i][2]))
+f.close()
+
+output_sorted = sorted(output, key = lambda x: float(x[2]))
+f2 = open('output_sums.txt', 'w')
+f3 = open('output_sorted.txt', 'w')
+for i in range(0, counter - 1):
+    f2.write("%s: %s\n" % (output_sorted[i][0], output_sorted[i][2]))
+    f3.write("%s: %s = %s\n" % (output_sorted[i][0], output_sorted[i][1], output_sorted[i][2]))
+f2.close()
+f3.close()
 
 # Final line to prevent from closing
-#input("Press Enter to finish")
+input("Files created. Press Enter to finish.")
     
