9b5e7af75af02a44d1b0d40bf6f4b24266b08c52
Umidah <alantsin92@gmail.com>
2017-12-08 16:35:23 -0800

    Refactored code and fixed bugs

--- Accurpress.py
+++ Accurpress.py
@@ -1,5 +1,6 @@
 from fractions import Fraction
-from past.types import basestring
+from itertools import combinations
+from test.test_bisect import Range
 
 # Helper function that takes a float and returns a fractional string representation of that number
 def display_fraction(x):
@@ -26,8 +27,8 @@ print ("Number of numbers: " + str(n))
 # If n = 0, uses test values
 if n == 0:
     # Adjust n and numbers for your own purposes
-    n = 11
-    numbers = [0.75, 1, 1.25, 2.25, 4, 5.25, 8.5, 12, 17, 48, 56]
+    n = 9
+    numbers = [0.75, 1, 1.25, 2.25, 4, 8.5, 17, 34, 75.25]
     print("Using test values:")
     for num in numbers:
         print(display_fraction(num))        
@@ -52,68 +53,48 @@ else:
                 
     for i in range(0, n):
         print("Number " + str(i + 1) + ": " + display_fraction(numbers[i]))
-
+        
         
 # Set up for output
 counter = 1
-# List to keep track of data. [0] is count, [1] is equation, [2] is sum
+# List to keep track of data. [0] is count, [1] is equation, [2] is sum, [3] is equation in fraction form
 output = []
 
-# Summation loops
-# First loop to keep track of total numbers to sum
-for numbersToAdd in range(1, n + 1):
-    # Second loop to keep track of the start index
-    for startIndex in range(0, n):
-        # If only one number, the sum is that number
-        if numbersToAdd == 1:
-            output.append(("Sum #%s:" % str(counter), display_fraction(numbers[startIndex]), numbers[startIndex]))
-            print("%s %s = %s" % (output[counter - 1][0], output[counter - 1][1], display_fraction(output[counter - 1][2])))
-            counter += 1
-        # Conditional to determine if this combination i and j would exceed the number of numbers
-        elif numbersToAdd + startIndex > n:
-            #print("Combination of numbersToAdd and startIndex exceed the maximum index")
-            break
-        # Else, add the numbers and store it
-        else:
-            tempSum = 0.0
-            tempSumString = ""
-            total = 0.0
-            # Third loop adds the numbers
-            for currentIndex in range(startIndex, numbersToAdd + startIndex):
-                # If the current index is less than the final number, store in a temporary sum
-                if currentIndex < numbersToAdd + startIndex - 1:   
-                    if currentIndex == startIndex:
-                        tempSumString = tempSumString + display_fraction(numbers[currentIndex])
-                    else:
-                        tempSumString = tempSumString + " + " + display_fraction(numbers[currentIndex])  
-                    tempSum += numbers[currentIndex]
+# First loop generates all combinations of subsets of certain length
+for r in range(1, n + 1):
+    subsets = combinations(numbers, r)
+    # Second loop iterates through each of those subsets
+    for subset in subsets:
+        equation = ""
+        equationFraction = ""
+        total = 0.0
+        # Third loop adds the numbers and saves the result
+        for i in range(0, r):
+            if i == 0:
+                equation = equation + str(subset[i])
+                equationFraction = equationFraction + display_fraction(subset[i])
+            else:
+                equation = equation + " + " + str(subset[i])
+                equationFraction = equationFraction + " + " + display_fraction(subset[i])
+            total = total + subset[i]
+        output.append(("Sum #%s: " % str(counter), equation, total, equationFraction))
+        print("%s%s = %s" % (output[counter - 1][0], output[counter - 1][1], output[counter - 1][2]))
+        counter += 1
 
-                    # print("Temp sum: " + str(tempSum))
-                # Otherwise, add tempSum to the final number and store it
-                else:
-                    for endIndex in range(currentIndex, n):
-                        tempSumStringFinal = tempSumString + " + " + display_fraction(numbers[endIndex])
-                        total = tempSum + numbers[endIndex]
-                        output.append(("Sum #%s: " % str(counter), tempSumStringFinal, total))
-                        print("%s %s = %s" % (output[counter - 1][0], output[counter - 1][1], display_fraction(output[counter - 1][2])))
-                        counter += 1
-                
 # Write results to files
 print("Now creating files for output")
 f = open('output_unsorted.txt', 'w')
 for i in range(0, counter - 1):
-    f.write("%s %s = %s\n" % (output[i][0], output[i][1], display_fraction(output[i][2])))
+    f.write("%s %s = %s\n" % (output[i][0], output[i][3], output[i][2]))
 f.close()
 
 output_sorted = sorted(output, key = lambda x: float(x[2]))
 f2 = open('output_sums.txt', 'w')
 f3 = open('output_sorted.txt', 'w')
 for i in range(0, counter - 1):
-    f2.write("%s %s\n" % (output_sorted[i][0], display_fraction(output_sorted[i][2])))
-    f3.write("%s %s = %s\n" % (output_sorted[i][0], output_sorted[i][1], display_fraction(output_sorted[i][2])))
+    f2.write("%s %s\n" % (output_sorted[i][0], output_sorted[i][2]))
+    f3.write("%s %s = %s\n" % (output_sorted[i][0], output_sorted[i][3], output_sorted[i][2]))
 f2.close()
 f3.close()
 
-# Final line to prevent from closing
-input("Files created. Press Enter to finish.")
-    
+input("Finished. Press Enter to end...")
